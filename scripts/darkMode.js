7d2bea3bdc5d7083309604bb321d85ea1f72092b
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2024-01-21 01:26:17 +0000

    amendments

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,14 +1,39 @@
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
+const card = document.querySelectorAll(".card");
+const a = document.querySelectorAll("a");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("🕶️")) {
         main.style.background = "#000";
         main.style.color = "#fff";
         modeButton.textContent = "🔆";
+
+        card.forEach(card => {
+            card.style.background = "#000";
+            card.style.color = "#fff";
+
+        });
+
+        a.forEach(a => {
+            a.style.color = "white";
+        });
+
+
+
     } else {
         main.style.background = "#eee";
         main.style.color = "#000";
         modeButton.textContent = "🕶️";
+
+        card.forEach(card => {
+            card.style.background = " #f0f0f0";
+            card.style.color = "black";
+        });
+
+        a.forEach(a => {
+            a.style.removeProperty("color");
+        });
+
     }
 });
